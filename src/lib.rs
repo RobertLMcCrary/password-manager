73ca273f63464e134b4dd62c1800a0c945767d16
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-05-14 17:32:49 -0400

    Store: Export security primitives

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,23 @@
+pub mod access_control;
+pub mod encryption;
 pub mod error;
 pub mod ffi;
 pub mod models;
 pub mod p2p;
+pub mod rekey;
 pub mod store;
 pub mod versioning;
 
+pub use access_control::{
+	AccessControl, AccessLevel, AdminAccess, Authorized, BranchPath, BranchSegment, BranchTarget,
+	EditAccess, GrantsAdmin, GrantsEdit, GrantsRead, GroupBranch, InMemoryAccessControl, ItemTarget,
+	PersonalBranch, PrincipalId, ReadAccess, RelayAccess, branch_storage_component,
+};
+pub use encryption::{AgeScrypt, EncryptionMethod, Locked, Unlocked};
 pub use error::{Error, Result};
 pub use models::{AccountName, Item, PasswordStore};
-pub use store::{ShareTicket, StoreBackend, StorePayload, VersionedEntry};
+pub use rekey::{MasterIdentity, MasterKeySet, OfflineSession, OfflineSessionPolicy};
+pub use store::{ShareTicket, StoreBackend, StoreChange, StorePayload, VersionedEntry};
 pub use versioning::{ChangeEntry, EntryHandle, PijulStore};
 
 uniffi::setup_scaffolding!();
